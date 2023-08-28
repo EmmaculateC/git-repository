4342529b2820333ed36c1098a406e7a97103b54b
emmahC <emmahbascalia999@gmail.com>
2023-08-28 11:42:12 +0300

    4th commit

--- change.js
+++ change.js
@@ -1,3 +1,3 @@
 console.log('change.js');
-console.log('Learning how to creating history Version 3');
+console.log('Learning how to creating history Version 4');
 console.log('I love new learning adventures');
